41ab5f23c3dd2337d1fc068ee46a25c1ce701ff0
Andreas Kofler <kofler_a@yahoo.de>
2021-06-02 09:35:02 +0200

    refactor: refactor cube and wireframe based on car example

--- examples/src/wireframe.js
+++ examples/src/wireframe.js
@@ -18,7 +18,7 @@ import ThreeJSOverlayView from '@ubilabs/threejs-overlay-view';
 
 import {getMapsApiOptions, loadMapsApi} from '../jsm/load-maps-api';
 
-const initialViewport = {
+const VIEW_PARAMS = {
   center: {
     lat: 53.55493986295417,
     lng: 10.007137126703523
@@ -42,22 +42,28 @@ const LOGO_ROTATION_Z = Math.PI / 12;
 const COLOR_CHANGE_DURATION = 30; // completes one hue cycle in x seconds
 
 async function main() {
+  const map = await initMap();
+
+  const overlay = new ThreeJSOverlayView(VIEW_PARAMS.center);
+  overlay.setMap(map);
+
+  initScene(overlay).then(() => overlay.requestRedraw());
+}
+
+async function initMap() {
   const {mapId} = getMapsApiOptions();
   await loadMapsApi();
 
-  const mapContainer = document.querySelector('#map');
-  const map = new google.maps.Map(mapContainer, {
+  return new google.maps.Map(document.querySelector('#map'), {
     mapId,
-    ...initialViewport
+    disableDefaultUI: true,
+    backgroundColor: 'transparent',
+    gestureHandling: 'greedy',
+    ...VIEW_PARAMS
   });
-
-  const overlay = new ThreeJSOverlayView(initialViewport.center);
-  overlay.setMap(map);
-
-  initScene(overlay, mapContainer).then(() => overlay.requestRedraw());
 }
 
-async function initScene(overlay, mapContainer) {
+async function initScene(overlay) {
   const scene = overlay.getScene();
 
   const wireframePath = [
@@ -123,7 +129,7 @@ function getWireframe(points) {
   lineGeometry.setPositions(positions);
   const lineMaterial = new LineMaterial({
     color: BUILDING_LINE_COLOR,
-    linewidth: 2
+    linewidth: 3
   });
 
   const line = new Line2(lineGeometry, lineMaterial);
